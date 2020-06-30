787ec9f71f046eed704f97fcac5b3313f01d3780
wesfb <ALEXBAIDEN@ICLOUD.COM>
2020-06-30 22:45:03 +0100

    testing workflow, too specfic atm, need to make functions

--- R/test_workflow.R
+++ R/test_workflow.R
@@ -10,19 +10,6 @@ library(tidyverse)
 library(purrr)
 library(stringr)
 
-regular = read_category_urls("https://www.mixesdb.com/w/MixesDB:Regular_categories",
-                             category_type = "")
-
-
-read_category_entries_safely <- safely(read_category_entries, otherwise = NA_real_)
-
-category_entries  <- map(regular, read_category_entries)
-
-category_entries_new <- map(regular, read_category_entries_safely)
-
-categor_results = transpose(category_entries_new)[["result"]]
-errors <- which(is.na(categor_results))
-
 
 extract_chart_entries <- function(page){
   html_nodes(page, "div.dplCatTable > ul a") %>%
@@ -35,7 +22,7 @@ read_category_entries <- function(home_page) {
 
   home_page_html <- read_html(home_page)
 
-  is_charts <- str_detect(home_page, pattern = "Charts")
+  is_charts <- str_detect(home_page, pattern = "Charts") #Chart pages have neeky url
 
   n_pages <- count_entries(home_page_html) %>%
     assert_pages()
@@ -54,6 +41,7 @@ read_category_entries <- function(home_page) {
 
 
 
+
   for (i in 1:(n_pages)) {
     page <- read_html(pages[[i]])
 
@@ -68,19 +56,32 @@ read_category_entries <- function(home_page) {
     } else {
 
       if (i == 1) {
-
         #next_page <- html_nodes(page, "#catcount a")[1]
-         next_page <- when(is_charts == T ~
-                             html_nodes(page, "span.plainlinks a")[1], ~
-                             html_nodes(page, "#catcount a")[1])
+        if (isTRUE(is_charts)){
+          next_page <- html_nodes(page, "span.plainlinks a")[1] #Chart pages seemed to work for this, you might find better html tho
+        } else{
+          next_page <- html_nodes(page, "#catcount a")[1]
+        }
+
       } else {
         #next_page <- html_nodes(page, "#catcount a")[2]
-         next_page <- when(is_charts == T ~
-                             html_nodes(page, "span.plainlinks a")[2], ~
-                             html_nodes(page, "#catcount a")[2])
+        if (isTRUE(is_charts)){
+          next_page <- html_nodes(page, "span.plainlinks a")[2]
+        } else{
+          next_page <- html_nodes(page, "#catcount a")[2]
+        }
       }
 
+      if(str_detect(html_attr(next_page, "href"), pattern = "www.mixesdb") ==  T) {
+
+        pages[[i+1]] <- paste0("https:", html_attr(next_page, "href"))
+      } else{
+
       pages[[i+1]] <- paste0("https://www.mixesdb.com", html_attr(next_page, "href"))
+      #^This works for majority of mixes, but some of the categories i.e beatport
+      #when i take the node already had //www.mixesdb.com/ in there, so i changed it.
+      }
+
     }
 
   }
@@ -92,15 +93,30 @@ read_category_entries <- function(home_page) {
 
 
 
+#Essential mix = regular [100]
+#Beat port = regular[23]
+single_category <- read_category_entries(regular[23])
 
-single_category <- read_category_entries(regular[1])
-
-test <- read_tracklist(paste0("https://www.mixesdb.com", foo[1]))
-
-map_dfr(regular_entries, read_tracklist)
-
-foo = unlist(l
-foo
+regular = read_category_urls("https://www.mixesdb.com/w/MixesDB:Regular_categories",
+                             category_type = "")
 
 
-map(regular_entries[1:2], read_category_entries)
+# read_category_entries_safely <- safely(read_category_entries, otherwise = NA_real_)
+# category_entries_new <- map(regular, read_category_entries_safely)
+
+category_entries  <- map(regular, read_category_entries)
+category_results = transpose(category_entries)[["result"]]
+errors <- which(is.na(category_results))
+
+regular_categories_results <- unlist(category_entries)
+
+tracklist_for_regular_category <- map_dfr(regular_categories_results[1], read_tracklist)
+
+read_regular_tracklists_safely <- safely(read_tracklist, otherwise = NA_real_)
+tracklist_for_regular_category <- map_dfr(regular_categories_results, read_regular_tracklists_safely)
+
+future::plan(multiprocess)
+regular_categories <- future_map(regular_categories_results[1:2],
+                                      read_regular_tracklists_safely,
+                                      .progress = T)
+regular_categories <- map(regular_entries, read_category_entries)
