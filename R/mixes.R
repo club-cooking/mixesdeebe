46860f76ac8cb5956299f0de18e6c0fca3feda90
wesfb <ALEXBAIDEN@ICLOUD.COM>
2020-09-02 00:01:05 +0100

    get_mix_meta function added Function also added into get_mix_entries to return detailed metadata.
    Tidy function to be done next

--- R/mixes.R
+++ R/mixes.R
@@ -34,6 +34,54 @@ get_mix_entries <- function(url) {
 
   }
 
-  paste0("https://www.mixesdb.com", unlist(mixes))
+  mix_urls <- paste0("https://www.mixesdb.com", unlist(mixes))
+
+  purrr::map(mix_urls, get_mix_meta)
+
+
 
 }
+
+get_mix_meta <- function(url){
+
+  page <- url
+  page_html <- xml2::read_html(page)
+
+
+  date_regex <- "\\d{4}-(?:0?[1-9]|1[012])-(?:0?[1-9]|[12][0-9]|3[01])*"
+
+  mix_meta <- list()
+
+
+  mix_title <- page_html %>%
+    extract_mix_title() %>%
+    stringr::str_replace_all("@", "-")
+
+
+  mix_meta[["date"]] <- anytime::anydate(str_extract(mix_title, date_regex))
+
+  mix_meta[["url"]] <- page
+
+  mix_title_date_excl <- stringr::str_remove(mix_title, date_regex) %>%
+    str_replace("-", "") %>%
+    str_trim()
+
+  mix_meta[["title"]] <- mix_title_date_excl
+
+  mix_title_split <- mix_title_date_excl %>%
+      str_split("-") %>%
+    unlist()
+
+  mix_meta[["artists"]] <- stringr::str_trim(mix_title_split[1])
+  mix_meta[["platform"]] <- stringr::str_trim(mix_title_split[2])
+
+  return(mix_meta)
+
+}
+
+get_mix_entries_tidy <- function(url){
+
+
+
+}
+
